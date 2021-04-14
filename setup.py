80845fbaa0ad98c321a4283752c89c0df9b6e571
kmikhed <kosti.mikhed@teck.com>
2021-04-14 18:58:30 +0300

    updated setup py

--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ with open("README.md", encoding="utf-8") as readme_file:
 
 setuptools.setup(
     name='draft_core_atqc',
-    version='0.0.1',
+    version='0.0.2',
     description='Draft framework core',
     long_description=readme,
     url='git@github.com:kosti-mikhed/draft-core-atqc.git',
+    author='Kosti Mikhed',
+    author_email='kosti.mikhed@teck.com',
+    license='unlicense',
     packages=setuptools.find_packages(exclude=['tests'])
 )
